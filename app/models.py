062c053cc0c343eceeeb078d208b6e0cfb0a181e
Patric Guggisberg <p.guggisberg@gmx.ch>
2023-11-27 22:13:52 +0100

    SQL dump

--- app/models.py
+++ app/models.py
@@ -64,7 +64,6 @@ class CertaintyScore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Integer, unique=True, index=True)
     description = db.Column(db.String(120))
-    is_structure_recognizable = db.Column(db.Boolean, default=False)
     sites = db.relationship('Site', backref='certainty_score', lazy='dynamic')
     
     def __repr__(self):
